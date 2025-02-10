40e290dfe9e41ab66790d431c3d83f53a307b5e4
russ <sayersr@amazon.com>
2025-02-10 16:06:59 +1100

    Added delete-access-entry to clean up script

--- 5-cleanup.sh
+++ 5-cleanup.sh
@@ -6,6 +6,9 @@ fi
 
 set -eo pipefail
 STACK=eks-lambda-python
+ROLE_ARN=$(aws cloudformation describe-stacks --stack-name eks-lambda-python --query "Stacks[0].Outputs[?OutputKey=='Role'].OutputValue" --output text)
+CLUSTER_NAME=$(cat cluster-name.txt)
+
 if [[ $# -eq 1 ]] ; then
     STACK=$1
     echo "Deleting stack $STACK"
@@ -34,6 +37,15 @@ while true; do
     read -p "Delete function log group (/aws/lambda/$FUNCTION)? (y/n)" response
     case $response in
         [Yy]* ) aws logs delete-log-group --log-group-name /aws/lambda/$FUNCTION; break;;
+        [Nn]* ) break;;
+        * ) echo "Response must start with y or n.";;
+    esac
+done
+
+while true; do
+    read -p "Delete access entry for $ROLE_ARN in $CLUSTER_NAME? (y/n)" response
+    case $response in
+        [Yy]* ) aws eks delete-access-entry --cluster $CLUSTER_NAME --principal-arn $ROLE_ARN; break;;
         [Nn]* ) break;;
         * ) echo "Response must start with y or n.";;
     esac
